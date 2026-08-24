127002f8b6c5a6f2f5f802b2c65976f1141b204d
Таня <berezov.tatyana@gmail.com>
2026-08-24 22:18:38 +0700

    feat: add auth field to Post model and create Comment model

--- blog/post/models.py
+++ blog/post/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Post(models.Model):
     title = models.CharField(
@@ -19,6 +21,16 @@ class Post(models.Model):
         verbose_name='Дата обновления'
     )
 
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Автор',
+        # user.posts.all()
+        related_name='posts',
+        null=True, # автор может быть пустым
+        blank=True #Может быть пустым
+    )
+
     def __str__(self):
         return self.title
 
@@ -27,3 +39,32 @@ class Post(models.Model):
         verbose_name_plural = 'Посты'
 
         ordering = ['-created_at'] #сортировка по дате поста
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        verbose_name='Пост',
+        related_name='comments'
+    )
+    text = models.TextField(verbose_name='Текст комментария')
+
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Автор',
+        related_name='comments'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True, #Фиксирует 1 раз при добавлении
+        verbose_name='Дата создания'
+    )
+
+    def __str__(self):
+        return f'Комментарий от {self.author} к "{self.posts.title}"'
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+        ordering = ['-created_at']
