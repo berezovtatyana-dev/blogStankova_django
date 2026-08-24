256bb1de2d8695bd16e98a24d3588a026e9747f7
Таня <berezov.tatyana@gmail.com>
2026-08-24 19:26:41 +0700

    feat: register Post in admin panel

--- blog/post/admin.py
+++ blog/post/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'update_at')
+    list_filter = ('created_at', 'update_at')
+    search_fields = ('title', 'content')
+    readonly_fields = ('created_at', 'update_at')
+    # группировка полей на странице редактирования
+    fieldsets = (
+        ('Основная информация', {
+            'fields': ('title', 'content')
+        }),
+        ('Даты', {
+            'fields': ('created_at', 'update_at'),
+            'classes': ('collapse',) #сворачиваемая секция
+        }),
+    )
