e743ecf50b67a9c35ac8e9cbea8a522dbe069aa7
Таня <berezov.tatyana@gmail.com>
2026-08-26 20:20:47 +0700

    feat: add create, edit, delete post urls

--- blog/post/urls.py
+++ blog/post/urls.py
@@ -7,4 +7,8 @@ app_name = 'post'
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:post_id>/', views.post_detail, name='post_detail')
+    path('post/create/', views.create_post, name='create_post'),
+    path('post/<int:post_id>/edit/', views.edit_post, name='edit_post')
+    path('post/<int:post_id>/delete/', views.delete_post, name='delete_post')
+
 ]
