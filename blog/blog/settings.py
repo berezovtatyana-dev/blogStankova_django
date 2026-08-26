b5da77261943f79a83d7a4741c89aef8e75ff58e
Таня <berezov.tatyana@gmail.com>
2026-08-26 21:16:36 +0700

    feat: add login, create/edit post templates

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -120,8 +120,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = 'post:post_list'
+LOGOUT_REDIRECT_URL = 'post:post_list'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
