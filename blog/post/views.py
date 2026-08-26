005f688b9b01440e7c5eafb95fe3d55cd6d036cf
Таня <berezov.tatyana@gmail.com>
2026-08-26 19:30:48 +0700

    feat: add post_detail view with comment form

--- blog/post/views.py
+++ blog/post/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Post
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import PostCreateForm, CommentForm
+
+from .models import Post, Comment
 
 def post_list(request):
     posts = Post.objects.all().order_by('-created_at')
@@ -12,6 +16,22 @@ def post_list(request):
 
 def post_detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
+    comments = post.comments.all().order_by('-created_at')
+    if request.method == 'POST':
+        # проверка на авторизацию свойство is_authenticated
+        if not request.user.is_authenticated:
+            messages.warning(request, 'Авторизуйтесь, чтобы оставить комментарий')
+            return redirect('login')
+        
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False) #commit - сохраняет данные в БД
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            messages.success(request, 'Комментарий добавлен')
+            return redirect('post:post_detail', post_id=post.id)
+
     context = {
         'post': post,
         'page_title': post.title
