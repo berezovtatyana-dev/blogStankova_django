ced98cda2d599ebbdd1c98af2a2bcb5ff1a7bc55
Таня <berezov.tatyana@gmail.com>
2026-08-26 21:21:40 +0700

    feat: add login, create/edit post templates

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('', include('post.urls')),
     # as_view используется для представлений-классов
     path('login/', auth_views.LoginView.as_view(
-        template_name='registration/login.html'), name='login')
+        template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(
         next_page='post:post_list'), name='logout')    
 ]
