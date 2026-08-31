ba1e699cce6a67e49759ddf1603d0ddf043b5171
Таня <berezov.tatyana@gmail.com>
2026-08-31 20:26:06 +0700

    feat: add template for registration

--- blog/userAcc/apps.py
+++ blog/userAcc/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class UseraccConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'userAcc'
+    verbose_name = 'Учетные записи пользователей'
