9e928d32bd667c8b57211f70b6849e04ad6cdfa1
Таня <berezov.tatyana@gmail.com>
2026-08-31 19:50:40 +0700

    feat: add register view with messages

--- blog/userAcc/views.py
+++ blog/userAcc/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
 
-# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            messages.success(request, 'Акквунт создан')
+            return redirect('login')
+        else: messages.error(request, 'Ошибки в форме')
+    else:
+        form = RegisterForm()
+    context = {
+        'form': form,
+        'page_title': 'Регистрацтя'
+    }
+    return render(request, 'userAcc/register.html', context)
