2af139525261865741e31deacd5d270b50720a73
Таня <berezov.tatyana@gmail.com>
2026-08-31 21:18:38 +0700

    feat: add signals for create and save profile

--- blog/userProfile/apps.py
+++ blog/userProfile/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class UserprofileConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'userProfile'
+    verbose_name = 'Профили пользователей'
+
+    def ready(self):
+        #  импортируем сигналы при готовности приложения
+        import userProfile.signals
