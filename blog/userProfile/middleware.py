9ecf03abd038e84e22ddc334150c7cb5924fa202
Таня <berezov.tatyana@gmail.com>
2026-09-02 20:04:15 +0700

    feat: add blockCheckMiddleware

---
+++ blog/userProfile/middleware.py
@@ -0,0 +1,27 @@
+from django.utils import timezone
+from django.contrib.auth import logout
+from django.contrib import messages
+from django.shortcuts import redirect
+
+class BlockCheckMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if request.user.is_authenticated:
+            profile = getattr(request.user, 'profile', None)
+            if profile and profile.is_curently_blocked():
+                if profile.blocked_until:
+                    date_str = profile.blocked_until.strftime('%d.%m.%Y %H:%M')
+                    messages.error(
+                        request,
+                        f'Ваш аккаунт заблокирован до {date_str}'
+                    )
+                else:
+                    messages.error(
+                        request,
+                        'Ваш аккаунт заблокирован навсегда'
+                    )
+                logout(request)
+                return redirect('login')
+            return self.get_response(request)
