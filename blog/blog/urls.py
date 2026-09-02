2b31d6ba423f7215ca80e47da95d7c0872f82570
Таня <berezov.tatyana@gmail.com>
2026-09-02 20:38:08 +0700

    feat: add configurations for profile

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -17,14 +17,20 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('post.urls')),
     path('register/', include('userAcc.urls')),
+    path('profile/', include('userProfile.urls')),
     # as_view используется для представлений-классов
     path('login/', auth_views.LoginView.as_view(
         template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(
         next_page='post:post_list'), name='logout')    
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
