5a2b7143bfb7ca70b8c34929866d70f5a45fb85a
Таня <berezov.tatyana@gmail.com>
2026-09-07 19:06:10 +0700

    feat: add delete comment url

--- blog/post/urls.py
+++ blog/post/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('post/create/', views.create_post, name='create_post'),
     path('post/<int:post_id>/edit/', views.edit_post, name='edit_post'),
     path('post/<int:post_id>/delete/', views.delete_post, name='delete_post')
+    path('comments/<int:comment_id>/delete/', views.delete_comment, name='delete_comment')
 ]
