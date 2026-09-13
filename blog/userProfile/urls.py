170c23da60dacd208397c8c3cfcde7d7f30d2aeb
Таня <berezov.tatyana@gmail.com>
2026-09-13 11:39:55 +0700

    feat: add urls for block user

--- blog/userProfile/urls.py
+++ blog/userProfile/urls.py
@@ -6,4 +6,6 @@ app_name = 'UserProfile'
 urlpatterns = [
     path('', views.profile, name='profile'),
     path('edit/', views.profile_edit, name='profile_edit'),
+    path('user/<int:user_id>/', views.user_profile_view, name='user_profile_view'),
+    path('user/<int:user_id>/block/', views.toggle_block_user, name='toggle_block_user'),
 ]
